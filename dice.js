7b1f3abb6f7db6f38157b591c3baf473f42eb1a8
rumbark <rahulumbarkarwipro@gmail.com>
2024-05-29 18:11:13 -0400

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,10 @@
+// alert("working");
+
+function rollDice() {
+var a= Math.floor(Math.random() * 6)+1;
+return a;
+ }
+var randomImage1 = "images/dice"+rollDice()+".png";
+var randomImage2 = "images/dice"+rollDice()+".png";
+ document.querySelector(".dice img1").setAttribute("src", randomImage1);
+ document.querySelector(".dice img2").setAttribute("src",randomImage2);
