b3a72d0cc784aec51b522885bf2449e99aeffe03
rumbark <rahulumbarkarwipro@gmail.com>
2024-05-29 18:15:01 -0400

    Add files via upload

--- dice.js
+++ dice.js
@@ -6,5 +6,7 @@ return a;
  }
 var randomImage1 = "images/dice"+rollDice()+".png";
 var randomImage2 = "images/dice"+rollDice()+".png";
- document.querySelector(".dice img1").setAttribute("src", randomImage1);
- document.querySelector(".dice img2").setAttribute("src",randomImage2);
+ document.querySelector(".dice .img1").setAttribute("src", randomImage1);
+ document.querySelector(".dice .img2").setAttribute("src",randomImage2);
+ 
+ document.querySelector("h1").innerHTML="Well Done. Now, refresh again!"
